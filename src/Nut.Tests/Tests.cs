5f00a569d9fc98515902693cf56c0e70a1d54d0f
DmitriyV <dmitriyps22@gmail.com>
2014-09-18 16:13:18 +0600

    tests added

--- src/Nut.Tests/Tests.cs
+++ src/Nut.Tests/Tests.cs
@@ -13,10 +13,31 @@
         }
 
         [Test]
-        public void Test()
+        public void Test1()
         {
             const decimal dec = 15.51m;
+            Assert.AreEqual("пятнадцать рублей пятьдесят одна копейка", dec.ToText(Currency.RUB, Language.Russian));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            const decimal dec = 15.52m;
             Assert.AreEqual("пятнадцать рублей пятьдесят две копейки", dec.ToText(Currency.RUB, Language.Russian));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            const decimal dec = 12.13m;
+            Assert.AreEqual("двенадцать рублей тринадцать копеек", dec.ToText(Currency.RUB, Language.Russian));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            const decimal dec = 2.02m;
+            Assert.AreEqual("два рубля две копейки", dec.ToText(Currency.RUB, Language.Russian));
         }
     }
 }
