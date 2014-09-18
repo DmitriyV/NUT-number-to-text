ea5b77ba58d2b9154d25e4bce0789ffa1806a144
DmitriyV <dmitriyps22@gmail.com>
2014-09-18 12:03:35 +0600

    assembly for tests added

---
+++ src/Nut.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace Nut.Tests
+{
+    using System;
+    using Constants;
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            var dec = new decimal(13.5);
+            Console.WriteLine(dec.ToText(Currency.RUB, Language.Russian));
+        }
+    }
+}
